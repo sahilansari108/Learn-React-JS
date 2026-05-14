a80d5ffca60f5da16460c924700622d4afdeec50
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-05-14 07:49:50 +0530

    vite Project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1> Sahil Ansari</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+    <h1>Sahil Ansari</h1>
+  )
+}
+
+export default App
